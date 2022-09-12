45e807f314d32405ae38405d0e4318b580cc0f9b
Kieran Harrison <17HarriK@fulford.york.sch.uk>
2022-09-12 14:55:16 +0100

    Added For and While loop

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -10,6 +10,24 @@ namespace Iteration
     {
         static void Main(string[] args)
         {
+
+        // Repeat 50 times
+        for (int i = 0; i < 50; i++)
+        {
+            Console.WriteLine("Hello World number " + (i+1));
+        }
+
+
+        // Repeat until the user says "yes"
+        bool nearlyThere = false;
+
+        while (nearlyThere == false)
+        {
+            Console.WriteLine("Are we there yet?");
+            nearlyThere = Console.ReadLine().ToLower() == "yes";
+
+        }
+        
         }
     }
 }
